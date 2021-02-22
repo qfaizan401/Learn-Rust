d7e0bf116a9cd0ef4e8e8867bc8d62d902b06309
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2021-02-22 21:52:21 +0500

    Hello to the Rust

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+D:\Faizan\Rust\Learn-Rust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+D:\Faizan\Rust\Learn-Rust\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+D:\Faizan\Rust\Learn-Rust\hello_world\target\debug\hello_world.exe: D:\Faizan\Rust\Learn-Rust\hello_world\src\main.rs
